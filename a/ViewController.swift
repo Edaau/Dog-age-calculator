4b1ee54e5024a61e63ca426e054eda8a14d2c6ac
Eduardo Pinheiro <duedugaranito@gmail.com>
2024-01-13 12:16:03 -0300

    Fixing bugs

--- a/ViewController.swift
+++ a/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    @IBAction func descobrirIdade(_ sender: Any) {
+        let idade = Int(campoIdadeCachorro.text!)!*7
+        
+        legendaResultado.text = "A idade do cachorro e: " + String(idade)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
 
